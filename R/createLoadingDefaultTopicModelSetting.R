d366b369840603c0c877e54e49657b0641c03012
parkdongsu <dongsu2005@naver.com>
2019-01-21 15:33:14 +0900

    devide create setting & fix DTM colnames, fix optimalTopic function

---
+++ R/createLoadingDefaultTopicModelSetting.R
@@ -0,0 +1,53 @@
+#' Custom createCoveriate Settings
+#'
+#' This function is Custom createCoveriate Settings.
+#' @param noteConceptId,existingTopicModel,topicModelExportRds,useDictionary,limitedMedicalTermOnlyLanguage,nGram,buildTopidModelMinFrac,buildTopidModelMaxFrac,optimalTopicValue,numberOfTopics,sampleSize
+#' @keywordsa createCovariateSetting
+#' @export
+#' @examples
+#' createLoadingDefaultTopicModelSetting()
+createLoadingDefaultTopicModelSetting <- function(noteConceptId = c(44814637),
+                                                  existingTopicModel = NULL,
+                                                  useDictionary=TRUE,
+                                                  limitedMedicalTermOnlyLanguage = c('KOR','ENG'),
+                                                  nGram = c(1L),
+                                                  buildTopidModelMinFrac = 0.001,
+                                                  buildTopidModelMaxFrac = 0.5,
+                                                  optimalTopicValue =FALSE,
+                                                  numberOfTopics=10L,
+                                                  sampleSize=-1){
+
+    if(sum(limitedMedicalTermOnlyLanguage %in% c('KOR','ENG')==FALSE) >=1){
+        stop("Implemented Language is c('KOR','ENG')")
+    }
+    covariateSettings <- list(noteConceptId = noteConceptId,
+                              sampleSize = sampleSize,
+                              ##useTextToVec
+                              useTextToVec = FALSE,
+                              ##TopicModeling
+                              useTopicModeling = TRUE,
+                              optimalTopicValue = optimalTopicValue,
+                              numberOfTopics = numberOfTopics,
+                              existingTopicModel = existingTopicModel,
+                              ##buildTopicModeling
+                              buildTopicModeling = FALSE,
+                              topicModelExportRds = NULL,
+                              ##TextToVec+TopicModeling
+                              useDictionary = useDictionary,
+                              limitedMedicalTermOnlyLanguage = limitedMedicalTermOnlyLanguage,
+                              nGram = nGram,
+                              buildTopidModelMinFrac = buildTopidModelMinFrac,
+                              buildTopidModelMaxFrac = buildTopidModelMaxFrac,
+                              ##Glove
+                              useGloVe = FALSE,
+                              latentDimensionForGlove = 0L,
+                              ##Autoencoder
+                              useAutoencoder = FALSE,
+                              latentDimensionForAutoEncoder = 0L)
+
+    attr(covariateSettings,'fun') <- 'getTopicFromNoteSettings'
+
+    class(covariateSettings) <- 'covariateSettings'
+
+    return(covariateSettings)
+}
